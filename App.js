b058e02ab3a12d2c32a2edfbeb6198e7f23f1bde
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-07 23:54:20 +0600

    react native icons

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Hello, React Native Icons </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
