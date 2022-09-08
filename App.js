7e223990950726a3edd8ad8d90b8e820b3ac9280
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-08 09:34:19 +0600

    react_native_vector_icons

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text} from 'react-native';
+
+// import so more icon from https://oblador.github.io/react-native-vector-icons/ and use .
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import Icon from 'react-native-vector-icons/AntDesign';
+// import Icon from 'react-native-vector-icons/Entypo';
+// import Icon from 'react-native-vector-icons/EvilIcons';
+// import Icon from 'react-native-vector-icons/Fontisto';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>Hello, React Native Icons </Text>
+        <Text>
+          React Native Icons from
+          https://oblador.github.io/react-native-vector-icons/{' '}
+        </Text>
+        <Text>
+          <Icon name="rocket" size={30} color="#647422" />
+          <Icon name="home" size={30} color="#900" />
+          <Icon name="thumbs-down" size={30} color="red" />
+          <Icon name="thumbs-up" size={30} color="#900" />
+          <Icon name="crop" size={30} color="blue" />
+          <Icon name="money" size={30} color="#900" />
+          <Icon name="bullhorn" size={30} color="green" />
+          <Icon name="upload" size={30} color="#900" />
+          <Icon name="facebook-square" size={30} color="blue" />
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
